35e2fda8b35f9072330334ac69fd0c2a6d5e2e63
Kyle Stoltzfus <computerwhiz33@gmail.com>
2025-12-03 01:39:31 -0500

    Allow users to see the Boost tab on the Analytics page

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -147,6 +147,7 @@ export default function Analytics() {
             >
               <Tab label="Summary" />
               <Tab label="Temperature" />
+              <Tab label="Boost" />
             </Tabs>
           </Box>
           {currentTab === 0 && (
@@ -181,6 +182,31 @@ export default function Analytics() {
                   {
                     data: records.map((record) => record.coolantTemperature),
                     label: "Coolant Temperature (°F)",
+                    showMark: false,
+                  },
+                ]}
+                height={400}
+                margin={{ left: 70, right: 20, top: 50, bottom: 70 }}
+                grid={{ vertical: true, horizontal: true }}
+              />
+            </Box>
+          )}
+          {currentTab === 2 && (
+            <Box sx={{ mt: 2, mb: 8, mr: 2, ml: 2, height: 400 }}>
+              <LineChart
+                xAxis={[
+                  {
+                    data: records.map((record) =>
+                      new Date(record.timestamp).getTime(),
+                    ),
+                    scaleType: "time",
+                    valueFormatter: (value) => dayjs(value).format("h:mm:ss A"),
+                  },
+                ]}
+                series={[
+                  {
+                    data: records.map((record) => record.boostPressure),
+                    label: "Boost Pressure (PSI)",
                     showMark: false,
                   },
                 ]}

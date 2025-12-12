803a4fa0f46e2eed7ee2a7e2f32ae1a0df8d65eb
Kyle Stoltzfus <computerwhiz33@gmail.com>
2025-12-12 00:26:57 -0500

    Update session name to avoid cookie collisions

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -2,7 +2,13 @@ import { Auth0Client } from "@auth0/nextjs-auth0/server";
 import { redirect } from "next/navigation";
 import { SessionData } from "@auth0/nextjs-auth0/types";
 
-export const auth0 = new Auth0Client();
+export const auth0 = new Auth0Client({
+  session: {
+    cookie: {
+      name: "obdtrak_session",
+    },
+  },
+});
 
 export const loginUrl = "/auth/login";
 export const logoutUrl = "/auth/logout";
